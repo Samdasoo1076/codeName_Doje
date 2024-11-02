4e31da2ad4fa21b3f1548f2c4be5d0679bdfecf4
MYJUN2007 <142660129+myjun0731@users.noreply.github.com>
2024-11-02 18:12:11 +0900

    Math
    계산기_찐_최종

--- 10-31/김명둔/math.java
+++ 10-31/김명둔/math.java
@@ -3,131 +3,126 @@ package test22;
 import java.util.Scanner;
 
 public class math {
+
+	private final static int max_num = 7;
 	static Scanner sc = new Scanner(System.in);
-	static int Array1[] = new int[7];
-	static int count = 0;
-	static int AddValue;
-	static int MinusValue;
-	static int MultValue;
-	static int DivisionValue;
-	static int Value;
-	static boolean Cmd = true;
+	private static int addresult;
+	private static int minusresult;
+	private static int multiplyresult;
+	private static int divideresult;
+	private static int result;
 
-	public static void Add() {
-		AddValue = Array1[0];
-		for (int i = 1; i < count; i++) {
-			AddValue += Array1[i];
+	public static void add(int[] array, int number) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i <= number; i++) {
+			addresult += array[i];
 		}
+		result();
 	}
 
-	public static void Minus() {
-		MinusValue = Array1[0];
-		for (int i = 1; i < count; i++) {
-			MinusValue -= Array1[i];
+	public static void minus(int[] array, int number) {
+		// TODO Auto-generated method stub
+		minusresult = array[0];
+		for (int i = 1; i <= number; i++) {
+			minusresult -= array[i];
 		}
-
+		result();
 	}
 
-	public static void Mult() {
-		MultValue = Array1[0];
-		for (int i = 1; i < count; i++) {
-			MultValue *= Array1[i];
+	public static void multiply(int[] array, int number) {
+		// TODO Auto-generated method stub
+		multiplyresult = array[0];
+		for (int i = 1; i <= number; i++) {
+			multiplyresult *= array[i];
 		}
-
+		result();
 	}
 
-	public static void Division() {
-		DivisionValue = Array1[0];
-		for (int i = 1; i < count; i++) {
-			DivisionValue /= Array1[i];
+	public static void divide(int[] array, int number) {
+		// TODO Auto-generated method stub
+		divideresult = array[0];
+		for (int i = 1; i <= number; i++) {
+			divideresult /= array[i];
 		}
-
+		result();
 	}
 
-	public static void NumberInput(int count2) {
-		if (Array1.length < count2) {
-			System.out.println("최대 입력 가능한 숫자의 개수는 7개 입니다.");
-		} else {
-			for (int i = 0; i < count2; i++) {
-				System.out.println((i + 1) + "번째 수를 입력하세요.");
-				Array1[i] = sc.nextInt();
-				count++;
-			}
-		}
-	}
-
-	public static void Input2() {
-		System.out.println("숫자 몇개 입력?");
-		int count2 = sc.nextInt();
-
-		System.out.println("연산자 선택) 1.덧셈 2.뺄셈 3.나눗셈 4.곱셈");
-
-		int calculation = sc.nextInt();
-
-		SC(count2, calculation);
-	}
-
-	public static void SC(int Num, int Num2) {
-
-		NumberInput(Num);
-
-		switch (Num2) {
-		case 1:
-			Add();
-			break;
-		case 2:
-			Minus();
-			break;
-		case 3:
-			Division();
-			break;
-		case 4:
-			Mult();
-			break;
-		}
-	}
-
-	public static void Output() {
-		Value += MultValue + DivisionValue + AddValue + MinusValue;
-
-		for (int i = 0; i < Array1.length; i++) {
-			Array1[i] = 0;
-		}
-
-		MultValue = 0;
-		DivisionValue = 0;
-		AddValue = 0;
-		MinusValue = 0;
-
+	public static void result() {
+		// TODO Auto-generated method stub
+		int Value = addresult + minusresult + multiplyresult + divideresult;
 		System.out.println("값 : " + Value);
+		addresult = 0;
+		minusresult = 0;
+		multiplyresult = 0;
+		divideresult = 0;
+	}
+
+	public static void input() {
+		// TODO Auto-generated method stub
+
+		int[] array = new int[max_num];
+		int count2 = 0;
+		System.out.println("몇개의 숫자를 입력하실건가요? : ");
+		int count = sc.nextInt();
+
+		if (count < max_num) {
+
+			for (int i = 0; i < count; i++) {
+				System.out.println((i + 1) + "번째 숫자를 입력해주세요.");
+				int number = sc.nextInt();
+				array[i] = number;
+				count2 = i;
+			}
+			System.out.println("연산자를 선택해주세요.");
+			System.out.println("1. 덧셈(+)");
+			System.out.println("2. 뺄셈(-)");
+			System.out.println("3. 곱셈(*)");
+			System.out.println("4. 나눗셈(/)");
+			int sign = sc.nextInt();
+			switch (sign) {
+			case 1:
+				add(array, count2);
+				break;
+			case 2:
+				minus(array, count2);
+				break;
+			case 3:
+				multiply(array, count2);
+				break;
+			case 4:
+				divide(array, count2);
+				break;
+			}
+		} else {
+			System.out.println("최대 입력 숫자의 개수는 7개 입니다.");
+		}
 	}
 
 	public static void main(String[] args) {
 
 		boolean cmd = true;
-		System.out.println("[계산기 메뉴]\n1.실행\n2.종료");
-		int cmd2 = sc.nextInt();
-
 		while (cmd) {
-			try {
-				switch (cmd2) {
-				case 1:
-					Input2();
-					Output();
-					System.out.println("[계산기 메뉴]\n1.실행\n2.종료");
-					cmd2 = sc.nextInt();
-					break;
-				case 2:
-					System.out.println("계산기를 종료합니다.");
-					sc.close();
-					cmd = false;
-					break;
-				default:
-					System.out.println("1과 2중에서만 입력해주세요");
-					break;
-				}
-			} catch (Exception e) {
-				System.out.println("숫자만 입력해주세요.");
+
+			System.out.println("[ 계산기 메뉴 ]");
+			System.out.println("1. 시작");
+			System.out.println("2. 종료");
+			int choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				input();
+				break;
+			case 2:
+				System.out.println(" 계산기를 종료합니다.");
+				cmd = false;
+				return;
+			default:
+				System.out.println("1과 2중에서 택하십시오.");
+				System.out.println("[ 계산기 메뉴 ]");
+				System.out.println("1. 시작");
+				System.out.println("2. 종료");
+				choice = sc.nextInt();
+				break;
 			}
 		}
 	}
